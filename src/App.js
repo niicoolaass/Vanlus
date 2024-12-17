3ff4354321fd4cb34bc8ad8b8a37fef186bf0f5d
niicoolaass <nicolasvieira147147@hotmail.com>
2024-12-16 23:57:49 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App () {
         <>
         
             <Routes>
-                <Route path='' element={<Home />}/>
+                <Route path='/' element={<Home />}/>
                 
                 <Route path='/Login' element={<Login />}/>
 
