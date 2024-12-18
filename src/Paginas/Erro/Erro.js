ab7d46f8a696894fa6c557e1b03710174473a989
niicoolaass <nicolasvieira147147@hotmail.com>
2024-12-17 23:43:20 -0300

    commit

--- src/Paginas/Erro/Erro.js
+++ src/Paginas/Erro/Erro.js
@@ -12,7 +12,7 @@ return(
     <div>
         <h1>PÁGINA NÃO ENCONTRADA</h1>
         <p>Desculpe mas a página que você procura não foi encontrada.</p>
-        <div><Link to='/'>HOME</Link></div>
+        <div><Link to='/Vanlus'>HOME</Link></div>
     </div>
     <img src={Tangled_line} style={{position:"absolute", zIndex:"99", bottom:"270px", right:"170px", height:"100px", transform:"rotate(-40deg)", filter:"drop-shadow( 13px -11px 4px rgba(0, 0, 0, 0.281))"}} alt="boy walking"/>
     <img src={Ghost} style={{position:"absolute", zIndex:"99", bottom:"120px", right:"30px", height:"200px", filter:"drop-shadow( 13px -11px 4px rgba(0, 0, 0, 0.281))"}} alt="boy walking"/>
